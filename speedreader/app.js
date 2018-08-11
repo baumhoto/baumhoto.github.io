9f0aeee8c957193162167bfe2c7b34dd365c2643
baumhoto <baumhoto@gmail.com>
2018-08-11 11:36:27 +0200

    speedreader changes

--- speedreader/app.js
+++ speedreader/app.js
@@ -64,13 +64,14 @@ function start() {
     readInterval = window.setInterval(flashWords, interval, preparedChunks);
     $('#start').html('Pause');
     $('body').data('reading', true);
+    $('#other, #options, #text-progress').fadeOut(500);
 }
 
 function stop() {
     window.clearInterval(readInterval);
     $('#start').html('Read!');
     $('body').data('reading', false);
-    $('#other, #options').fadeIn(500);
+    $('#other, #options, #text-progress').fadeIn(500);
 }
 
 function flashWords(array) {
@@ -132,7 +133,6 @@ $(document).ready(function() {
                 $('#reading-screen, #new').fadeIn(250);
             });
             $('h1').animate({height: 0, opacity: 0}, 500);
-            $('#other, #options').fadeOut(500);
             $('#merge').attr('disabled', true).parent().css('opacity', 0.5);
         } else {
             stop();
